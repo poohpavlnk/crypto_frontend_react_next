60304634554d43524b32e218e4b18656b7028c0a
papo ------- <avaradam2020@gmail.com>
2023-03-05 17:29:01 +0700

    Enhance docstring documentation

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ setConfig(require('./next.config'))
 require('./server')# Change 1 on 2020-04-29
 # Change 1 on 2020-04-30
 # Change 0 on 2020-05-09
+# Change 1 on 2020-05-14
+# Change 2 on 2023-03-05
